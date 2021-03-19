7364de8adb84c4d25ea7500a799f2b89318d489c
callie rogers <callie1013@gmail.com>
2021-03-19 18:18:15 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,9 @@
 # Define the variable board below.
-board = Array.new(" ", " ", " ", " ", " ", " ", " ", " ", " ")
+board = Array.new 3[row]
+
+row = Array.new(" ", " ", " ")
+
+"position 1" = row [0]
+"position 2" = row [1]
+"position 3" = row [2]
+"----------"
