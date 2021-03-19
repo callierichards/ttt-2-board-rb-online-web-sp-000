89f13dd4ec577084e26a7f0c84e1363feea69249
callie rogers <callie1013@gmail.com>
2021-03-19 18:33:54 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
 # Define the variable board below.
-board_variable = board
-def board = Array.new [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-puts row = "   |   |   "
-puts separator = "----------"
+board = Array.new [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+row = "   |   |   "
+separator = "----------"
+
+def board
+puts row
+puts separator
 puts row
 puts separator
 puts row
